073e0e5c2233782bdc43b826c257cb0effc7d359
Balpakna123455 <92869182+Balpakna123455@users.noreply.github.com>
2021-10-20 21:18:22 +0600

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,20 @@
+# my fast python tool
+
+import os
+
+prinrt ('termux repekar')
+
+os.system ('pkg update && pkg upgrade')
+os.system ('pkg install python')
+os.system ('pkg install python2')
+os.system ('pkg install bash')
+os.system ('pkg install git')
+os.system ('pkg install ruby')
+os.system ('pkg install openssh')
+os.system ('pkg install wget')
+os.system ('pkg install pip')
+os.system ('pkg install pip2')
+os.system ('pip2 install requests')
+os.system ('pip2 install mechanize')
+os.system ('pkg install php')
+os.system ('pip2 install lolcat')
